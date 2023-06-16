de520741622fa73d806d2596960b8eb6bba2f838
yarin <yarin066@gmail.com>
2023-06-16 15:18:01 +0300

    Complete adding and removing item actions

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,16 +1,30 @@
+import { useState, useEffect } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import { useContext } from "react";
 import CartContext from "../../store/cart-context";
 export const HeaderCartButton = (props) => {
+  const [isBtnBumped, setBtnBumped] = useState(false);
   const cartCtx = useContext(CartContext);
-
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClass = `${classes.button} ${isBtnBumped ? classes.bump : ""}`;
+  useEffect(() => {
+    if (!items.length) return;
+    setBtnBumped(true);
+    const timer = setTimeout(() => {
+      setBtnBumped(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClass} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -7,9 +7,51 @@ const defaultCartState = {
 };
 const cartReducer = (state, action) => {
   if (action.type === "ADD_CART_ITEM") {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    let updatedItems;
+
+    const existingCartItem = state.items[existItemIndex];
+    if (existingCartItem) {
+      let updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+
+      updatedItems = [...state.items];
+      updatedItems[existItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
+  if (action.type === "REMOVE_CART_ITEM") {
+    const existItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingItem = state.items[existItemIndex];
+
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems = [];
+
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existItemIndex] = updatedItem;
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
